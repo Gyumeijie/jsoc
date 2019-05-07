5ee07e14520a6c3c58e575bf3ba98522936d0449
Gyumeijie <18170879819@163.com>
2019-05-07 22:41:45 +0800

    refactor: meta tweaks

--- index.js
+++ index.js
@@ -2,14 +2,13 @@
 
 const isPrimitive = (val) => typeof val !== 'object' || val === null;
 
-const typeRegExp = /\[object (\w+)\]/;
 const getTypeOf = (obj) => {
   const type = Object.prototype.toString.call(obj);
-  return typeRegExp.exec(type)[1];
+  // [object xxx] typename starts at 8
+  return type.slice(8, -1);
 };
 
 const compositeType = ['Object', 'Array', 'Map', 'WeakMap', 'Set', 'WeakSet'];
-const isCompositeType = (obj) => compositeType.includes(getTypeOf(obj));
 
 // Handler to clone Date object
 const cloneDate = (date) => new Date(date.getTime());
@@ -51,14 +50,6 @@ const initializeClonedObject = (obj) => {
   return initVal;
 };
 
-const hasBeenCloned = (objRefs, obj) => {
-  if (objRefs.indexOf(obj) === -1) {
-    objRefs.push(obj);
-    return false;
-  }
-  return true;
-};
-
 const clone = (obj) => {
   // Return the obj if it is actually a primitive
   if (isPrimitive(obj)) return obj;
@@ -67,14 +58,17 @@ const clone = (obj) => {
   const objRefs = [];
   const map = new Map();
   const cloning = (target) => {
-    if (hasBeenCloned(objRefs, target)) {
+    // If target has been cloned
+    if (objRefs.indexOf(target) !== -1) {
       return map.get(target);
     }
+    objRefs.push(target);
 
     const cloned = initializeClonedObject(target);
     map.set(target, cloned);
 
-    if (isCompositeType(target)) {
+    const type = getTypeOf(target);
+    if (compositeType.includes(type)) {
       // Loops through the properties of any composite object
       for (const key in target) {
         cloned[key] = isPrimitive(target[key])
@@ -83,28 +77,28 @@ const clone = (obj) => {
       }
 
       // Additional operations for `Map`
-      if (getTypeOf(target) === 'Map') {
+      if (type === 'Map') {
         for (const [key, value] of target.entries()) {
           cloned.set(key, isPrimitive(value) ? value : cloning(value));
         }
       }
 
       // Additional operations for `Set`
-      if (getTypeOf(target) === 'Set') {
+      if (type === 'Set') {
         for (const key of target.keys()) {
           cloned.add(isPrimitive(key) ? key : cloning(key));
         }
       }
 
       // The `WeakSet` and `WeakMap` cann't be cloned
-      if (['WeakSet', 'WeakMap'].includes(getTypeOf(target))) {
-        throw new Error(`${getTypeOf(target)} can not be cloned`);
+      if (['WeakSet', 'WeakMap'].includes(type)) {
+        throw new Error(`${type} can not be cloned`);
       }
 
       return cloned;
     }
     // Process other types such as `Date`, `RegExp`, `Promise`
-    return cloneHandlers[getTypeOf(target)](target, cloning);
+    return cloneHandlers[type](target, cloning);
   };
   return cloning(obj);
 };
